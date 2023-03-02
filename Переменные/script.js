54a65445d9b8893fb17d605a53e8d8521d6f35f6
sergei-connect <fresh787@yandex.ru>
2023-03-02 20:11:59 +0800

    2.4 Переменные

---
+++ Переменные/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+//Task 1
+
+let admin;
+let name;
+name = "Джон";
+admin = name;
+
+alert(admin);
+
+//Task 2
+
+let myPlanet = "Земля";
+let currentUser = "Иван";
+
+//Task 3
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
