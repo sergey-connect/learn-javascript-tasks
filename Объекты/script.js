cfa350836f13e1407aef41701d20ef7264e01f91
sergei-connect <fresh787@yandex.ru>
2023-03-11 12:35:26 +0800

    4.1 Объекты

---
+++ Объекты/script.js
@@ -0,0 +1,140 @@
+//Task 1
+
+/*
+Напишите код, выполнив задание из каждого пункта отдельной строкой:
+
+Создайте пустой объект user.
+Добавьте свойство name со значением John.
+Добавьте свойство surname со значением Smith.
+Измените значение свойства name на Pete.
+Удалите свойство name из объекта.
+*/
+
+let user = {};
+
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+
+delete user.name;
+
+//=======================================
+
+//Task 2
+
+/*
+Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+Должно работать так:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+*/
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+alert(isEmpty(schedule)); // false
+
+//==================================
+
+//Task 3
+
+/*
+Можно ли изменить объект, объявленный с помощью const? Как вы думаете?
+
+const user = {
+  name: "John"
+};
+
+// это будет работать?
+user.name = "Pete";
+*/
+
+//Да, будет работать
+
+//Task 4
+
+/*
+У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+
+Если объект salaries пуст, то результат должен быть 0.
+*/
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key]
+}
+
+alert(sum);
+
+//Task 5
+
+/*
+Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+Например:
+
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// после вызова функции
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Обратите внимание, что multiplyNumeric не нужно ничего возвращать. Следует напрямую изменять объект.
+
+P.S. Используйте typeof для проверки, что значение свойства числовое.
+*/
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == "number") {
+      obj[key] *= 2
+    }
+  }
+}
+
+multiplyNumeric(menu);
