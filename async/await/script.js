e816b48ce31de416e2a31d3b5b4d4873b27ddec3
sergei-connect <fresh787@yandex.ru>
2023-05-20 23:30:47 +0200

    async/await

---
+++ async/await/script.js
@@ -0,0 +1,161 @@
+//Task 1
+
+/*
+Перепишите, используя async/await
+Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+*/
+/*
+async function loadJson(url) {
+  let response = await fetch(url)
+
+  if (response.status == 200) {
+    return response.json();
+  }
+  throw new Error(response.status);
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+*/
+
+//Task 2
+
+/*
+Перепишите, используя async/await
+Ниже пример из раздела Цепочка промисов, перепишите его, используя async/await вместо .then/catch.
+
+В функции demoGithubUser замените рекурсию на цикл: используя async/await, сделать это будет просто.
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new HttpError(response);
+      }
+    })
+}
+
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+function demoGithubUser() {
+  let name = prompt("Введите логин?", "iliakan");
+
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then(user => {
+      alert(`Полное имя: ${user.name}.`);
+      return user;
+    })
+    .catch(err => {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+        return demoGithubUser();
+      } else {
+        throw err;
+      }
+    });
+}
+
+demoGithubUser();
+*/
+/*
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url)
+
+  if (response.status == 200) {
+    return response.json();
+  }
+
+  throw new HttpError(response);
+}
+
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+async function demoGithubUser() {
+  let user;
+  while (true) {
+    let name = prompt("Введите логин?", "iliakan");
+
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+      } else {
+        throw err;
+      }
+    };
+  }
+
+  alert(`Полное имя: ${user.name}.`);
+  return user;
+}
+
+demoGithubUser();
+*/
+
+//Task 3
+
+/*
+Вызовите async–функцию из "обычной"
+Есть «обычная» функция. Как можно внутри неё получить результат выполнения async–функции?
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  // ...что здесь написать?
+  // чтобы вызвать wait() и дождаться результата "10" от async–функции
+  // не забывайте, здесь нельзя использовать "await"
+}
+P.S. Технически задача очень простая, но этот вопрос часто задают разработчики, недавно познакомившиеся с async/await.
+*/
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  return 10;
+}
+
+function f() {
+  wait().then(result => console.log(result))
+  // ...что здесь написать?
+  // чтобы вызвать wait() и дождаться результата "10" от async–функции
+  // не забывайте, здесь нельзя использовать "await"
+}
+
+f();
